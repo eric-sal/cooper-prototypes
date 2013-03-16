a39f5a75bce4663d7cf6ea710bca6093a1c9648a
Eric Salczynski <eric@wehaventthetime.com>
2013-03-16 17:11:26 -0400

    Added KindredSpirit demo

---
+++ kindred-sprite-demo/Assets/Scripts/Character/Player/PlayerAnimation.js
@@ -0,0 +1,74 @@
+#pragma strict
+
+public enum Animations { StandRight, SlideRight, WalkRight, JumpRight, StandLeft, SlideLeft, WalkLeft, JumpLeft, PlayerDeath };
+
+private var _character : Character;
+private var _characterController : CharacterController2D;
+private var _sprite : Sprite;
+private var _currentAnimation : Animations;
+
+// Use this for initialization
+function Start() {
+	_character = GetComponent(Character);
+	_characterController = GetComponent(CharacterController2D);
+	_sprite = GetComponent(Sprite);
+
+}
+
+// Update is called once per frame
+function FixedUpdate() {
+	if (_character.IsDead()) {
+		if (_currentAnimation != Animations.PlayerDeath) {
+			_currentAnimation = Animations.PlayerDeath;
+			_sprite.ShowFrame(0);
+
+			var currentPosition : Vector3 = transform.position;
+			var apex : Vector3 = currentPosition + (Vector3.up * 112);
+			
+			// Calling Player.Respawn() oncomplete probably isn't the best bet in the long run.
+			// We not only want to respawn the player, but reset the level.
+			// Plan: Add a method to SceneController called Reset, and add a Reset method to
+			// every object that can/needs to be reset after player death.
+			//iTween.MoveTo(gameObject, {'path': [apex, currentPosition], 'easetype': 'linear', 'time': 1, 'oncomplete': 'Respawn' });
+		}
+	} else {
+		if (_character.IsFacingRight()) {
+			if (_characterController.IsWalking()) {
+				if (_currentAnimation != Animations.WalkRight) {
+					_currentAnimation = Animations.WalkRight;
+					_sprite.ShowFrame(13);
+					//_sprite.Play("WalkRight");
+				}
+			} else if (_characterController.IsJumping()) {
+				if (_currentAnimation != Animations.JumpRight) {
+					_currentAnimation = Animations.JumpRight;
+					_sprite.ShowFrame(2);
+				}
+			} else {
+				if (_currentAnimation != Animations.StandRight) {
+					_currentAnimation = Animations.StandRight;
+					_sprite.ShowFrame(7);
+				}
+			}
+		} else {
+			// player is facing left
+			if (_characterController.IsWalking()) {
+				if (_currentAnimation != Animations.WalkLeft) {
+					_currentAnimation = Animations.WalkLeft;
+					_sprite.ShowFrame(9);
+					//_sprite.Play("WalkLeft");
+				}
+			} else if (_characterController.IsJumping()) {
+				if (_currentAnimation != Animations.JumpLeft) {
+					_currentAnimation = Animations.JumpLeft;
+					_sprite.ShowFrame(1);
+				}
+			} else {
+				if (_currentAnimation != Animations.StandLeft) {
+					_currentAnimation = Animations.StandLeft;
+					_sprite.ShowFrame(6);
+				}
+			}
+		}
+	}
+}
---
+++ kindred-sprite-demo/Assets/Scripts/SceneController.js
@@ -0,0 +1,6 @@
+#pragma strict
+
+// Put any scene-related static vars in this script.
+// DOES NOT have to be attached as a component on a GameObject.
+
+public static var GRAVITY : float = -600;
