535ded88f25914645a94539a31b28bb7a61c5331
Eric Salczynski <eric@wehaventthetime.com>
2013-04-02 23:07:18 -0400

    Added simple bg parallax scrolling example with 2nd camera

--- kindred-sprite-demo/Assets/Scripts/CameraController.js
+++ kindred-sprite-demo/Assets/Scripts/CameraController.js
@@ -3,12 +3,16 @@
 public var player : Player;
 private var _sprite : Sprite;
 private var _camera : Transform;
+private var _bgCamera : Transform;
 
 function Start () {
 	_camera = GameObject.FindWithTag("MainCamera").transform;
+    _bgCamera = GameObject.FindWithTag("BGCamera").transform;
 	_sprite = player.GetComponent(Sprite);
 }
 
 function Update () {
 	_camera.position.x = _sprite.position.x;
+    _bgCamera.position.x  = _sprite.position.x * 0.5;
+
 }
